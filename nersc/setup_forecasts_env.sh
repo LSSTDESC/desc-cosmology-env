56b5415fab23c5a10238a5bc3644beae00cde3e6
Heather Kelly <heather999kelly@gmail.com>
2023-08-07 09:21:13 -0400

    Create setup_forecasts_env.sh

---
+++ nersc/setup_forecasts_env.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+module load PrgEnv-gnu
+module load cpu
+module load cray-mpich-abi/8.1.25
+module load evp-patch
+
+
+# For cosmosis and firecrown.  Should try to find a better way to set these
+#export CSL_DIR=$CONDA_PREFIX/lib/python3.10/site-packages/cosmosis/cosmosis-standard-library
+#export FIRECROWN_SITE_PACKAGES=$CONDA_PREFIX/lib/python3.10/site-packages
+#export FIRECROWN_DIR=$DESC_LSST_INSTALL_DIR/firecrown
+#export FIRECROWN_EXAMPLES_DIR=$FIRECROWN_DIR/examples
+
+# Fixes missing support in the Perlmutter libfabric:
+# https://docs.nersc.gov/development/languages/python/using-python-perlmutter/#missing-support-for-matched-proberecv
+export MPI4PY_RC_RECV_MPROBE=0
+
+# Tries to prevent cosmosis from launching any subprocesses, since that is 
+# not allowed on Perlmutter.
+export COSMOSIS_NO_SUBPROCESS=1
