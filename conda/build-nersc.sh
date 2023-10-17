16769407614f2fe152c9eb5c463afc6feb359686
Heather Kelly <heather@slac.stanford.edu>
2023-10-17 10:24:43 -0700

    adding augur

--- conda/build-nersc.sh
+++ conda/build-nersc.sh
@@ -40,9 +40,9 @@ then
 fi
 
 mkdir -p $curBuildDir
-#cp conda/packlist.txt $curBuildDir
+cp conda/condapack.txt $curBuildDir
 #cp conda/post-conda-build.sh $curBuildDir
-#cp conda/piplist.txt $curBuildDir
+cp conda/pippack.txt $curBuildDir
 cp nersc/setup_forecasts_env.sh $curBuildDir
 cp nersc/sitecustomize.py $curBuildDir
 sed -i 's|$1|'$curBuildDir'|g' $curBuildDir/setup_forecasts_env.sh
@@ -57,42 +57,120 @@ url="$url/Mambaforge-Linux-x86_64.sh"
 curl -LO "$url"
 
 bash ./Mambaforge-Linux-x86_64.sh -b -p $curBuildDir/py
-source $curBuildDir/py/etc/profile.d/conda.sh
-conda activate base
+#source $curBuildDir/py/etc/profile.d/conda.sh
+#conda activate base
+source $curBuildDir/py/bin/activate
+#
+export PYTHONNOUSERSITE=1
 
-conda create -y --name desc-forecasts
+conda create -y --name desc-forecasts compilers
 conda activate desc-forecasts
 
-mamba install -c conda-forge -y mpich=4.0.3=external_* 
+python -m pip cache purge
+
+mamba install -c conda-forge -y mpich=4.1.2=external_* 
+ 
+cd $curBuildDir
+#mamba install -c conda-forge -y --file ./condapack.txt
 
 # Install firecrown in dev mode this will pull in CCL,cobaya, cosmosis
 git clone https://github.com/LSSTDESC/firecrown.git
-conda env update --name desc-forecasts -f firecrown/environment.yml
+#echo -e "\nmpich=4.1.2=external_*" >> firecrown/environment.yml
+mamba env update --name desc-forecasts -f firecrown/environment.yml
+conda activate desc-forecasts
+mamba install -c conda-forge -y mpich=4.1.2=external_* 
 source ${CONDA_PREFIX}/bin/cosmosis-configure
-# Download and build the CosmoSIS standard library
-# in a directory under the CosmoSIS python directory
-cosmosis-build-standard-library 
-#-i
+cosmosis-build-standard-library
 
-conda env config vars set CSL_DIR="${PWD}/cosmosis-standard-library" FIRECROWN_DIR="${PWD}/firecrown" PYTHONPATH="${PWD}/firecrown/build/lib"
+export CSL_DIR=${PWD}/cosmosis-standard-library
+export FIRECROWN_DIR=${PWD}/firecrown
+export PYTHONPATH=${FIRECROWN_DIR}/build/lib
 
-cd ${PWD}/firecrown
+cd firecrown
 python setup.py build
 python -m pytest -vv
 
+cd $curBuildDir
+
+#export COSMOSIS_SRC_DIR=${CONDA_PREFIX}/lib/python3.11/site-packages/cosmosis
+#export COSMOSIS_ALT_COMPILERS=1
+#export CC=gcc
+#export CXX=g++
+#export FC=gfortran
+#export MPIFC=ftn
+#export MPIF90=ftn
+#export COSMOSIS_OMP=1
+
+# Environment variables for compilation
+#export LAPACK_LINK="-L$CRAY_LIBSCI_PREFIX_DIR/lib -lsci_gnu"
+#export GSL_DIR=${CONDA_PREFIX}
+#export FFTW_LIBRARY=${CONDA_PREFIX}/lib
+#export GSL_INC=$GSL_DIR/include
+#export GSL_LIB=$GSL_DIR/lib
+#export FFTW_INCLUDE_DIR=${CONDA_PREFIX}/include
+#export CFITSIO_DIR=${CONDA_PREFIX}
+#export CFITSIO_INC=$CFITSIO_DIR/include
+#export CFITSIO_LIB=$CFITSIO_DIR/lib
+    
+
+
+# Download and build the CosmoSIS standard library
+# in a directory under the CosmoSIS python directory
+#cosmosis-build-standard-library 
+#-i
+#git clone https://github.com/joezuntz/cosmosis-standard-library
+#cd cosmosis-standard-library
+#make
+
+#cosmosis-build-standard-library -i
+
+#conda env config vars set CSL_DIR="${PWD}/cosmosis-standard-library" FIRECROWN_DIR="${PWD}/firecrown" PYTHONPATH="${PWD}/firecrown/build/lib" AUGUR_DIR="${PWD}/augur" PYTHONNOUSERSITE=1
+#conda env config vars set CSL_DIR="$CONDA_PREFIX/lib/python3.10/site-packages/cosmosis/cosmosis-standard-library" FIRECROWN_DIR="${PWD}/firecrown" PYTHONPATH="${PWD}/firecrown/build/lib" AUGUR_DIR="${PWD}/augur" PYTHONNOUSERSITE=1
+conda env config vars set CSL_DIR="${PWD}/cosmosis-standard-library" FIRECROWN_DIR="${PWD}/firecrown" PYTHONPATH="${PWD}/firecrown/build/lib" AUGUR_DIR="${PWD}/augur" PYTHONNOUSERSITE=1
+
+#pip install --no-cache-dir -r ./pippack.txt
+
+# Grab firecrown source so we have the examples subdirectory
+#firecrown_ver=$(conda list firecrown | grep firecrown|tr -s " " | cut -d " " -f 2)
+#echo $firecrown_ver
+#curl -LO https://github.com/LSSTDESC/firecrown/archive/refs/tags/v$firecrown_ver.tar.gz
+#tar xvzf v$firecrown_ver.tar.gz
+# Set up a common directory name without version info to set FIRECROWN_DIR more easily
+#ln -s firecrown-$firecrown_ver firecrown
+
+#cd ${PWD}/firecrown
+#python setup.py build
+#export FIRECROWN_DIR="$curBuildDir/firecrown"
+#python -m pytest -vv
+
+#cd $curBuildDir
+#git clone https://github.com/LSSTDESC/TJPCov.git
+#cd TJPCov
+#python -m pip install .\[full\]
+
+cd $curBuildDir
+git clone https://github.com/LSSTDESC/augur.git
+pip install --no-deps augur/
+#cd augur
+#python setup.py install
+
+cd $curBuildDir
+
 #mamba install -c conda-forge -y --file ./packlist.txt
-#pip install --no-cache-dir -r ./piplist.txt
 
+python -m compileall $curBuildDir/py
 conda clean -y -a 
-
 
 # Additional build steps
 #bash ./post-conda-build.sh
 
+#CONDA_PREFIX="" pip install -v  --no-cache cosmosis
+#cd $curBuildDir
+#git clone https://github.com/joezuntz/cosmosis-standard-library
+#cd cosmosis-standard-library
+#make
 
-python -m compileall $curBuildDir
-
-conda config --set env_prompt "(desc-forecasts-env-$1)" --system
+conda config --set env_prompt "(desc-forecasts-env-$1)" --env
 
 conda env export --no-builds > $curBuildDir/desc-forecasts-env-nersc-$CI_PIPELINE_ID-nobuildinfo.yml
 conda env export > $curBuildDir/desc-forecasts-nersc-$CI_PIPELINE_ID.yml
